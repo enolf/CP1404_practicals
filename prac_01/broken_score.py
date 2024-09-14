091b59b50917442fc4f33bc6429ba411a028b177
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-09-14 13:23:40 +1000

    completed required practicals

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -1,19 +1,15 @@
 """
-CP1404/CP5632 - Practical
+CP1404/CP5632 - Practical - Florian N Eisen
 Broken program to determine score status
 """
 
-# TODO: Fix this!
-
 score = float(input("Enter score: "))
-if score < 0:
+
+if score < 0 or score > 100:
     print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
+elif score >= 90:
     print("Excellent")
-if score < 50:
+elif score >= 50:
+    print("Passable")
+elif score < 50:
     print("Bad")
